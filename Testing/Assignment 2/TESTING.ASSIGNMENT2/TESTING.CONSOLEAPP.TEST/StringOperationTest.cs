95eceffc544a0ccf799590f26acac6226abe3f7b
saurabh singh <Ss520553@gmail.com>
2021-03-29 10:03:18 +0530

    testing assignment fixed

--- Testing/Assignment 2/TESTING.ASSIGNMENT2/TESTING.CONSOLEAPP.TEST/StringOperationTest.cs
+++ Testing/Assignment 2/TESTING.ASSIGNMENT2/TESTING.CONSOLEAPP.TEST/StringOperationTest.cs
@@ -8,103 +8,145 @@ namespace TESTING.CONSOLEAPP.TEST
 {
     public class StringOperationTest
     {
-        [Fact]
-        public void Test_ChangeCase()
+
+        [Theory]
+        [InlineData("Upper", "upper")]
+        [InlineData("UPPER", "upper")]
+        [InlineData("lower", "LOWER")]
+        public void Test_ChangeCase(string inputString, string result)
         {
-            // Arrange
-            var inputString = "Upper";
-            var outputString = "upper";
+
             // Act
             var newString = inputString.ChangeCase();
+
             // Assert
-            Assert.Equal(newString, outputString);
+            Assert.Equal(newString, result);
+
         }
-        [Fact]
-        public void Test_ChangeToTitleCase()
+
+        [Theory]
+        [InlineData("ram MCA RAM Bca BbA", "Ram MCA RAM Bca Bba")]
+        [InlineData("Ram RAm MCA", "Ram Ram MCA")]
+        [InlineData("abc bCA bcOm MVc MVC", "Abc Bca Bcom Mvc MVC")]
+        public void Test_ChangeToTitleCase(string inputString, string result)
         {
-            // Arrange
-            var inputString = "ram MCA RAM Bca BbA";
-            var outputString = "Ram MCA RAM Bca Bba";
+            
             // Act
             var newString = inputString.ChangeToTitleCase();
+            
             // Assert
-            Assert.Equal(newString, outputString);
+            Assert.Equal(newString, result);
+        
         }
+
         [Fact]
         public void Test_IsLowerCaseString()
         {
+
             // Arrange
             var inputString = "ramsingh";
+            
             // Act
             var newString = inputString.IsLowerCaseString();
+            
             // Assert
             Assert.True(newString);
+        
         }
+
         [Fact]
         public void Test_IsUpperCaseString()
         {
+
             // Arrange
             var inputString = "RAM";
+            
             // Act
             var newString = inputString.IsUpperCaseString();
+            
             // Assert
             Assert.True(newString);
+        
         }
+
         [Fact]
         public void Test_DoCapitalize()
         {
+
             // Arrange
             var inputString = "ram";
             var outputString = "Ram";
+            
             // Act
             var newString = inputString.DoCapitalize();
+            
             // Assert
             Assert.Equal(newString,outputString);
+        
         }
+
         [Theory]
         [InlineData("123", true)]
         [InlineData("123c", false)]
+        [InlineData("453", true)]
+        [InlineData("abc", false)]
         public void Test_IsValidNumericValue(string inputString,bool result)
         {
-            // Arrange
-            
+
             // Act
             var newString = inputString.IsValidNumericValue();
+            
             // Assert
             Assert.Equal(newString, result);
+        
         }
-        [Fact]
-        public void Test_RemoveLastCharacter()
+
+        [Theory]
+        [InlineData("abcd", "abc")]
+        [InlineData("123c", "123")]
+        [InlineData("Ram Singh", "Ram Sing")]
+        [InlineData("SRB", "SR")]
+        public void Test_RemoveLastCharacter(string inputString, string result)
         {
-            // Arrange
-            var inputString = "Ram Singh";
-            var outputString = "Ram Sing";
+            
             // Act
             var newString = inputString.RemoveLastCharacter();
+            
             // Assert
-            Assert.Equal(newString, outputString);
+            Assert.Equal(newString, result);
+        
         }
-        [Fact]
-        public void Test_WordCount()
+
+        [Theory]
+        [InlineData("abcd", 1)]
+        [InlineData("Saurabh singh dikhit",3)]
+        [InlineData("Ram Singh", 2)]
+        [InlineData("SRB", 1)]
+        public void Test_WordCount(string inputString, int count)
         {
-            // Arrange
-            var inputString = "Ram Singh";
-            var count = 2;
+            
             // Act
             var newString = inputString.WordCount();
+            
             // Assert
             Assert.Equal(newString, count);
+        
         }
+
         [Fact]
         public void Test_StringToInteger()
         {
+
             // Arrange
             var inputString = "123";
             var output = 123;
+            
             // Act
             var newString = inputString.StringToInteger();
+            
             // Assert
             Assert.Equal(newString, output);
+        
         }
     }
 }
