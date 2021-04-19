0ac4b0e949f7392fb874b6b360d33bf1d93d41c3
saurabh singh <Ss520553@gmail.com>
2021-04-19 19:19:01 +0530

    nunit assignment 7 added

---
+++ Nunit Assignment/Nunit Assignment7/NunitAssignment7/NunitAssignment7/Concrete/Functions.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitAssignment7.Concrete
+{
+    public class Functions
+    {
+
+        // while
+        public int NumberAddition(int n)
+        {
+            int i = 1, addition = 0;
+            while (i <= n)
+            {
+                addition += i;
+                i++;
+            }
+            return addition;
+
+        }
+
+        // switch
+        public string GenderSelection(int number)
+        {
+            string gender = "";
+            switch (number)
+            {
+                case 1:
+                    gender = "Male";
+                    break;
+                case 2:
+                    gender = "Female";
+                    break;
+                case 3:
+                    gender = "Others";
+                    break;
+                default:
+                    gender = "Invalid Gender";
+                    break;
+            }
+            return gender;
+        }
+
+        // if else
+        public int LargeNumber(int a,int b)
+        {
+            if (a>b)
+                return a;
+            else
+            {
+                return b;
+            }
+        }
+
+        //foreach ....
+        public int ListLength()
+        {
+            List<int> numbers = new List<int>() { 1, 2, 5, 7, 8, 10 };
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+
+        //for loop
+        public int CountVowels(string name)
+        {
+            int count=0;
+            for (int i = 1; i < name.Length; i++)
+            {
+                if (name[i] == 'A' || name[i] == 'E' || name[i] == 'I' || name[i] == 'O' || name[i] == 'U' ||
+                    name[i] == 'a' || name[i] == 'e' || name[i] == 'i' || name[i] == 'o' || name[i] == 'u')
+                    count += 1;
+            }
+            return count;
+        }
+
+        // NullReferenceException
+        public void NullReferenceException(string s1)
+        {
+            if (s1 == null)
+                throw new NullReferenceException("Null Reference Exception");
+        }
+
+        // Devide by zero
+        public void DivideByZero(int num1, int num2)
+        {
+            int result = 0;
+            try
+            {
+                result = num1 / num2;
+            }
+            catch (DivideByZeroException)
+            {
+                throw new DivideByZeroException("Divide By Zero Exception");
+            }
+        }
+        //Array Index Out Of Bound 
+        public void ArrayIndexOutOfBound()
+        {
+            try
+            {
+                string name = "Saurabh Singh";
+                Console.WriteLine(name[name.Length]);
+            }
+            catch (Exception)
+            {
+
+                throw new IndexOutOfRangeException("Array Index out of Bound");
+            }
+        }
+
+        //Async
+        public async Task<int> CountVowelAsync(string name)
+        {
+            int count = 0;
+            for (int i = 1; i < name.Length; i++)
+            {
+                if (name[i] == 'A' || name[i] == 'E' || name[i] == 'I' || name[i] == 'O' || name[i] == 'U' ||
+                    name[i] == 'a' || name[i] == 'e' || name[i] == 'i' || name[i] == 'o' || name[i] == 'u')
+                    count += 1;
+            }
+            return count;
+        }
+    }
+}
---
+++ Nunit Assignment/Nunit Assignment7/NunitAssignment7/NunitAssignment7/UnitTest.cs
@@ -0,0 +1,153 @@
+using NUnit.Framework;
+using NunitAssignment7.Concrete;
+using System;
+using System.Threading.Tasks;
+
+namespace NunitAssignment7
+{
+    public class Tests
+    {
+        private Functions _function;
+        [SetUp]
+        public void Setup()
+        {
+            // Arrange
+            _function = new Functions();
+        }
+
+        // While Loop Test case
+        // Positive Test Case
+        [Test]
+        public void NumberAddition_Test_Positive()
+        {
+            // Act
+            int result = _function.NumberAddition(10);
+
+            // Assert
+            Assert.AreEqual(55, result);
+        }
+
+        // While Loop Test case
+        // Positive Test Case
+        [Test]
+        public void NumberAddition_Test_Negative()
+        {
+            // Act
+            int result = _function.NumberAddition(10);
+
+            // Assert
+            Assert.AreNotEqual(10, result);
+        }
+
+        // Switch case Tests
+        [TestCase(1, "Male")]
+        [TestCase(2, "Female")]
+        [TestCase(3, "Others")]
+        [TestCase(4, "Invalid Gender")]
+        public void GenderSelection_Test(int choice, string gender)
+        {
+            //Act
+            string result = _function.GenderSelection(choice);
+
+            //Assert
+            Assert.AreEqual(gender, result);
+        }
+
+        // If else tests
+        [TestCase(23,34,34)]
+        [TestCase(23,12,23)]
+        public void LargeNumber_Test(int a,int b,int large)
+        {
+            //Act
+            int result = _function.LargeNumber(a,b);
+
+            //Assert
+            Assert.AreEqual(large, result);
+        }
+
+        // foreach Test case
+        // Positive Test Case
+        [Test]
+        public void ListLength_Test_Positive()
+        {
+            // Act
+            int result = _function.ListLength();
+
+            // Assert
+            Assert.AreEqual(33, result);
+        }
+
+        // foreach Test case
+        // Positive Test Case
+        [Test]
+        public void ListLength_Test_Negative()
+        {
+            // Act
+            int result = _function.ListLength();
+
+            // Assert
+            Assert.AreNotEqual(30, result);
+        }
+
+        // for loop tests
+        [TestCase("Ram Singh",2)]
+        [TestCase("Ram Singh Dikhit",4)]
+        public void CountVowels_Test(string name,int vowelcount)
+        {
+            //Act
+            int result = _function.CountVowels(name);
+
+            //Assert
+            Assert.AreEqual(vowelcount, result);
+        }
+
+        // Null Reference Exception
+        [Test]
+        public void NullReferenceException_Test()
+        {
+            //Act
+            var exceptionResult = Assert.Throws<NullReferenceException>(() => _function.NullReferenceException(null));
+
+            //Assert
+            Assert.AreEqual("Null Reference Exception", exceptionResult.Message);
+        }
+
+        // Divide by zero Exception
+        [Test]
+        public void DivideByZero_Test()
+        {
+            //Act
+            var exceptionResult = Assert.Throws<DivideByZeroException>(() => _function.DivideByZero(12,0));
+
+            //Assert
+            Assert.AreEqual("Divide By Zero Exception", exceptionResult.Message);
+        }
+
+
+        // Array Index out of bound
+        [Test]
+        public void ArrayIndexOutOfBound_Test()
+        {
+            //Act
+            var exceptionResult = Assert.Throws<IndexOutOfRangeException>(() => _function.ArrayIndexOutOfBound());
+
+            //Assert
+            Assert.AreEqual("Array Index out of Bound", exceptionResult.Message);
+        }
+
+        [Test]
+        public async Task CountVowelAsync_Test_Positive()
+        {
+            var result = await _function.CountVowelAsync("saurabh singh dikhit");
+            Assert.AreEqual(6, result);
+        }
+
+        [Test]
+        public async Task CountVowelAsync_Test_Negative()
+        {
+            var result = await _function.CountVowelAsync("saurabh singh dikhit");
+            Assert.AreNotEqual(7, result);
+        }
+
+    }
+}
